4d0b9cad655b49510fd086fef11b6201796eac86
vaibhavgoyal09 <79615436+vaibhavgoyal09@users.noreply.github.com>
2022-06-17 21:28:26 +0530

    Edit Chat Table

--- data/model/chat.py
+++ data/model/chat.py
@@ -4,19 +4,12 @@ from app.database import Base
 from data.model.relations.user_chat import user_chat
 
 
-class ChatType(Enum):
-    one_to_one = 0
-    group = 1
-
-
 class ChatTable(Base):
     __tablename__ = "chat"
 
     id = Column(Integer, primary_key=True)
-    type = Column(Integer, nullable=False, default=ChatType.one_to_one.value)
     unseen_message_count = Column(Integer, nullable=False, default=0)
     last_message_id = Column(Integer, nullable=True)
-    group_id = Column(Integer, nullable=True)
 
     users = orm.relationship(
         "UserTable", secondary="user_chat", backref='chats'  
