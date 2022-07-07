a13e008414e3daeb9161943a8570674ccb25a63c
vaibhavgoyal09 <79615436+vaibhavgoyal09@users.noreply.github.com>
2022-07-07 18:46:49 +0530

    Fixed Issues With Chat Routes

--- app/api/v1/chat_routes.py
+++ app/api/v1/chat_routes.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, Depends
+from app.model.user import User
 from app.service.chat_service import ChatService
 from app.service.firebase_service import get_current_user
-from app.model.user import User
+from app.utils.result_wrapper import *
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import ORJSONResponse
+
+router = APIRouter(prefix="/chat", tags=["chat"])
 
 
-router = APIRouter(prefix='/chat', tags=['chat'])
-
-
-@router.get('/all')
+@router.get("/all")
 async def get_all_chats(
-    service: ChatService = Depends(),
-    user: User = Depends(get_current_user)
+    service: ChatService = Depends(), user: User = Depends(get_current_user)
 ):
-    await service.handle_get_all_chats(user)
+    result: ResultWrapper = await service.get_all_chats(user)
+    if isinstance(result, Error):
+        raise HTTPException(status_code=result.code, detail=result.message)
+    else:
+        return ORJSONResponse(result)
--- app/service/chat_service.py
+++ app/service/chat_service.py
@@ -1,24 +1,27 @@
-from fastapi import Depends, HTTPException
-from typing import List
-from data.repository.chat_repository import ChatRepository
-from app.model.user import User
 import traceback
+from typing import List
+
 from app.model.response.chat import Chat
+from app.model.user import User
+from app.utils.result_wrapper import *
+from data.repository.chat_repository import ChatRepository
+from fastapi import Depends, HTTPException
 
 
 class ChatService:
-
     def __init__(self, chat_repository: ChatRepository = Depends()):
         self.chat_repository = chat_repository
 
-    async def handle_get_all_chats(self, user_self: User):
+    async def get_all_chats(self, user_self: User) -> ResultWrapper[List[Chat]]:
         try:
             chats_objs = await self.chat_repository.get_all_chats_for_user(user_self)
 
-            chats: List[Chat] = [] 
+            chats: List[Chat] = []
 
             for chat_obj in chats_objs:
-                users = await self.chat_repository.get_all_user_ids_for_chat(chat_obj.id)
+                users = await self.chat_repository.get_all_users_for_chat(
+                    chat_obj.id
+                )
 
                 if len(users) > 2:
                     raise Exception("Users can't be more than two.")
@@ -30,16 +33,17 @@ class ChatService:
                         remote_user = user
 
                 chat = Chat(
-                    id = chat_obj.id,
+                    id=chat_obj.id,
                     remote_user_id=remote_user.id,
                     remote_user_name=remote_user.name,
                     remote_user_profile_image_url=remote_user.profile_image_url,
                     last_message_id=chat_obj.last_message_id,
-                    unseen_message_count=chat_obj.unseen_message_count
+                    unseen_message_count=chat_obj.unseen_message_count,
                 )
 
                 chats.append(chat)
+            return chats
 
-        except:
+        except Exception as e:
             print(traceback.print_exc())
-            return []
+            return Error(message=e)
