9355214f7a4db0f8eef515a4ac80a0ea0975193d
vaibhavgoyal09 <vgoyal98145@gmail.com>
2023-02-27 13:23:22 +0530

    Bug Fixes

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = os.getenv("PROJECT_NAME", "whatsapp-clone-backend")
+    IS_DEBUG_MODE: bool = bool(os.getenv("IS_DEBUG_MODE", 1))
 
     MAX_CONNECTIONS_COUNT = int(os.getenv("MAX_CONNECTIONS_COUNT", 10))
     MIN_CONNECTIONS_COUNT = int(os.getenv("MIN_CONNECTIONS_COUNT", 10))
@@ -19,16 +20,17 @@ class Settings(BaseSettings):
     MONGO_DATABASE_NAME: str = os.getenv("MONGO_DATABASE_NAME", "whatsapp")
     MONGO_HOST: str = os.getenv("MONGO_HOST", "localhost")
     MONGO_PORT: int = int(os.getenv("MONGO_PORT", 27017))
-    DATABASE_URI: Optional[DatabaseURL] = None
+    DATABASE_URI = None
 
     @validator("DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-        return DatabaseURL(
+        if values.get("IS_DEBUG_MODE"):
+            return DatabaseURL(
             f'mongodb://{values.get("MONGO_INITDB_ROOT_USERNAME")}:{values.get("MONGO_INITDB_ROOT_PASSWORD")}@{values.get("MONGO_HOST")}:{values.get("MONGO_PORT")}/{values.get("MONGO_DATABASE_NAME")}?authSource=admin'
         )
-
+        else:
+            return v
+        
     class Config:
         case_sensitive = True
         env_file = ".env"
