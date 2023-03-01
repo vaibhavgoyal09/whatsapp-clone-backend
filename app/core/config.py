f62fec361174a8e74a63bfaf5f3010e71e8e8960
vaibhavgoyal09 <vgoyal98145@gmail.com>
2023-03-01 19:04:25 +0530

    Bug Fixes

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, List, Optional
 from pydantic import AnyHttpUrl, BaseSettings, validator
 from functools import lru_cache
-from databases import DatabaseURL
 import os
 
 
@@ -20,12 +19,12 @@ class Settings(BaseSettings):
     MONGO_DATABASE_NAME: str = os.getenv("MONGO_DATABASE_NAME", "whatsapp")
     MONGO_HOST: str = os.getenv("MONGO_HOST", "localhost")
     MONGO_PORT: int = int(os.getenv("MONGO_PORT", 27017))
-    DATABASE_URI = None
+    DATABASE_URI: Optional[str] = None
 
     @validator("DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if values.get("IS_DEBUG_MODE"):
-            return DatabaseURL(
+            return (
             f'mongodb://{values.get("MONGO_INITDB_ROOT_USERNAME")}:{values.get("MONGO_INITDB_ROOT_PASSWORD")}@{values.get("MONGO_HOST")}:{values.get("MONGO_PORT")}/{values.get("MONGO_DATABASE_NAME")}?authSource=admin'
         )
         else:
